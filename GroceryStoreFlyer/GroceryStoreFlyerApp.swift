e657805a5ebdc83420f9595070309ac641acd9b1
Tiffany Zhang <tzhang@lcs.on.ca>
2025-02-03 15:08:51 -0500

    Initial Commit

---
+++ GroceryStoreFlyer/GroceryStoreFlyerApp.swift
@@ -0,0 +1,17 @@
+//
+//  GroceryStoreFlyerApp.swift
+//  GroceryStoreFlyer
+//
+//  Created by xinyu zhang on 2025-02-03.
+//
+
+import SwiftUI
+
+@main
+struct GroceryStoreFlyerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
